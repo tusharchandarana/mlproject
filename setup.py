f44d4b581e6c5fa9441659c53c5be17937c5e0d2
Tushar <acd48216@gmail.com>
2026-08-30 13:30:02 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements = [req.replace("\n"," ") for req in requirements]
+        requirements = [req.strip() for req in requirements]
         
         if '-e .' in requirements:
             requirements.remove('-e .')
